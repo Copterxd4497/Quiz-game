2b9acf744847b98e5bb0fabf2b4db6f5e9afd3e8
Copter <copterpanyphon@gmail.com>
2025-10-15 00:55:53 +0700

    first_change

---
+++ quiz game/dist/index.js
@@ -0,0 +1,20 @@
+// // Guard access to `document` so this file won't crash when executed in Node.
+// const inputOne =
+//   typeof document !== "undefined"
+//     ? (document.getElementById("inputOne") as HTMLInputElement | null)
+//     : null;
+// const buttonOne =
+//   typeof document !== "undefined"
+//     ? (document.getElementById("buttonOne") as HTMLButtonElement | null)
+//     : null;
+// const showText =
+//   typeof document !== "undefined"
+//     ? (document.getElementById("showText") as HTMLElement | null)
+//     : null;
+export {};
+// console.log("Everything is OK Sir");
+// buttonOne?.addEventListener("click", () => {
+//   const text = inputOne?.value ?? "";
+//   if (showText) showText.textContent = text;
+// });
+//# sourceMappingURL=index.js.map
